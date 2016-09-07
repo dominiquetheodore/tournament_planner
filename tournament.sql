b829f041b5ec11be635e74db382aa02f75307395
dominique theodore <dominique@MacBook-Pro.local>
2016-09-07 18:29:31 +0400

    commented tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -14,6 +14,8 @@ CREATE TABLE players (ID SERIAL PRIMARY KEY, name TEXT);
 
 CREATE TABLE matches (ID SERIAL PRIMARY KEY, winner INTEGER REFERENCES players (ID), loser INTEGER REFERENCES players (ID));
 
+-- helpful views to check out query results
+
 CREATE VIEW standings as select players.id, players.name, count(a.winner) as wins, count(a.winner) + count(b.loser) as matches from players left join matches as a on players.id=a.winner left join matches as b on players.id=b.loser group by players.id order by wins DESC;
 
 CREATE VIEW win_total as SELECT players.name, count(players.name) as win_total from players, matches WHERE players.id=matches.winner group by players.name order by win_total desc;
